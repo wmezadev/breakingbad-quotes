990b2070dbf97533bf851bf62382da0b53f08527
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-10 19:12:59 -0500

    Feat - add fetch API Breaking Bad

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column-reverse;
+`
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #FFFFFF;
+  margin-top: 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  &:hover {
+    cursor: pointer;
+    color: black;
+    background-color: #FFFFFF;
+  }
+`
 
 function App() {
+
+  const fetchAPI = async () => {
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    let quote = await api.json()
+    console.log(quote)
+  }
   return (
-    <h1>
-      
-    </h1>
+    <Container>
+      <Button
+        type="button"
+        onClick={ () => fetchAPI() }
+      >
+        Get Quote
+      </Button>
+    </Container>
   );
 }
 
