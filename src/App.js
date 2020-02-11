fdab3a5b281543af3d445107b1e9761a99b43ccf
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-10 19:28:38 -0500

    Feat - Add styled components to Quote and useEffect to async fetch to API

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
+import Quote from './components/Quote';
 
 const Container = styled.div`
   display: flex;
   align-items: center;
   padding-top: 5rem;
-  flex-direction: column-reverse;
+  flex-direction: column;
 `
 
 const Button = styled.button`
   background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
   background-size: 300px;
-  font-family: Arial, Helvetica, sans-serif;
-  color: #FFFFFF;
+  font-family:  Arial, Helvetica, sans-serif;
+  color: #fff;
   margin-top: 3rem;
+  padding: 1rem 3rem;
   font-size: 2rem;
   border: 2px solid black;
+  transition: background-size .8s ease;
   &:hover {
     cursor: pointer;
     color: black;
     background-color: #FFFFFF;
+    background-size: 400px;
   }
 `
 
 function App() {
 
+  const [quote, setQuote] = useState({})
+
   const fetchAPI = async () => {
     const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
     let quote = await api.json()
-    console.log(quote)
+    setQuote(quote[0])
   }
+
+  useEffect( () => {
+    fetchAPI()
+  }, []);
+
+
   return (
     <Container>
+      <Quote
+        quote={quote}
+      />
       <Button
         type="button"
         onClick={ () => fetchAPI() }
